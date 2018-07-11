12b9125b2229b44531d3b5b8577ff0db74ee6f9a
Jeff Goodrich <jgoodr30@gmail.com>
2018-07-11 18:28:04 -0400

    First commit

---
+++ app/src/main/java/com/example/jeffg/Utils/MovieUrlUtils.java
@@ -0,0 +1,63 @@
+package com.example.jeffg.Utils;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+
+public class MovieUrlUtils {
+
+    /**
+     * Put your api key here ****************
+     */
+    public static final String API_KEY = "37a185f3d9ad9b6d0c3fbadd59bd9353";
+    /**
+     * **************************************
+     */
+    private static final String LOG_TAG = MovieUrlUtils.class.getSimpleName();
+    private static final String MOVIE_QUERY_API = "api_key";
+    private static final String MOVIE_BASE_URL = "https://api.themoviedb.org/3/movie/";
+
+
+    public static URL buildUrl(String movieUrl) {
+
+        Uri uri = Uri.parse(MOVIE_BASE_URL)
+                .buildUpon()
+                .appendPath(movieUrl)
+                .appendQueryParameter(MOVIE_QUERY_API, API_KEY)
+                .build();
+        URL url = null;
+        try {
+            url = new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problems create url", e);
+        }
+
+        return url;
+    }
+
+    public static String getResponseFromHttp(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream inputStream = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(inputStream);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
+
